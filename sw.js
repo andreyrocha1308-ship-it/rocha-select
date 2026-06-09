40d87d9f148184ed3684e472146456a902e03318
andreyrocha1308-ship-it <andrey.rocha1308@gmail.com>
2026-06-09 17:50:26 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'rocha-select-v2';
+const assets = [
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
