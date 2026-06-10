129715de5be8eee6caa5d05264793eaf2867e518
andreyrocha1308-ship-it <andrey.rocha1308@gmail.com>
2026-06-10 13:57:03 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,9 @@ const CACHE_NAME = 'rocha-select-v2';
 const assets = [
   './index.html',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://cdn.jsdelivr.net/npm/chart.js'
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
 ];
 
 self.addEventListener('install', e => {
